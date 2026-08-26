5d4aa22ce3adb016ae4d17ad019a45715bf903a8
clouchheim <carter.louchheim@example.com>
2026-08-26 10:23:54 -0600

    load dot env added was needed

--- a360_csv_to_table.py
+++ a360_csv_to_table.py
@@ -25,8 +25,8 @@ Set SB_DRY_RUN=false once you've confirmed the JSON looks right.
 """
 
 import os
-from loading import load_dotenv
-load_dotenv()  # load .env file if present
+from dotenv import load_dotenv
+load_dotenv()
 import csv
 import io
 import json
